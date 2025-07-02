d8ddaf41cd5df5617164eab20d82ddd24bdc1aaf
Phirs12 <gabrielomar244@gmail.com>
2025-07-02 11:00:41 -0600

    portafolio

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function goToMateria(materia) {
+    if (materia === 'materia1') {
+        window.location.href = "materia1.html"; // Redirige a la página de Materia 1
+    } else if (materia === 'materia2') {
+        window.location.href = "materia2.html"; // Redirige a la página de Materia 2
+    }
+}
